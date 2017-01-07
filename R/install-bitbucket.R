22fed790a380c8b268b9229d7d78690cb8405127
Imanuel Costigan <i.costigan@me.com>
2017-01-08 08:39:14 +1100

    Modify add_metadata() for BB

--- R/install-bitbucket.R
+++ R/install-bitbucket.R
@@ -174,16 +174,16 @@ checkout_bitbucket <- function(p, host = "bitbucket.org", credentials = NULL, ve
         d <- file.path(d, p$subdir)
     }
     p$sha1 <- git2r::commits(gitrepo)[[1]]@sha
-    try(add_metadata(d, p, verbose = verbose), silent = TRUE)
+    try(add_bb_metadata(d, p, verbose = verbose), silent = TRUE)
     return(d)
 }
 
-add_metadata <- function(pkgdir, p, verbose = FALSE) {
+add_bb_metadata <- function(pkgdir, p, verbose = FALSE) {
     ghitmsg(verbose, message(sprintf("Adding metadata to DESCRIPTION for package %s...", p$pkgname)))
     desc <- file.path(pkgdir, "DESCRIPTION")
     dcf <- read.dcf(desc)
-    meta <- c("github", p$user, p$pkgname, p$sha1)
-    dimn <- list(NULL, c("RemoteType", "GithubUsername", "GithubRepo", "GithubSHA1"))
+    meta <- c("bitbucket", p$user, p$pkgname, p$sha1)
+    dimn <- list(NULL, c("RemoteType", paste0("Bitbucket", c("Username", "Repo", "SHA1"))))
     metamat <- matrix(meta, nrow = 1, dimnames = dimn)
     dcf <- cbind(dcf, metamat)
     write.dcf(dcf, file = desc)
