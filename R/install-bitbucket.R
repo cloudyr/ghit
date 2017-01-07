41dba333d60302ebcfd4d34fa2ae63645f1caa77
Imanuel Costigan <i.costigan@me.com>
2017-01-08 08:21:29 +1100

    Copy add_metadata() prior to editing

--- R/install-bitbucket.R
+++ R/install-bitbucket.R
@@ -177,3 +177,15 @@ checkout_bitbucket <- function(p, host = "bitbucket.org", credentials = NULL, ve
     try(add_metadata(d, p, verbose = verbose), silent = TRUE)
     return(d)
 }
+
+add_metadata <- function(pkgdir, p, verbose = FALSE) {
+    ghitmsg(verbose, message(sprintf("Adding metadata to DESCRIPTION for package %s...", p$pkgname)))
+    desc <- file.path(pkgdir, "DESCRIPTION")
+    dcf <- read.dcf(desc)
+    meta <- c("github", p$user, p$pkgname, p$sha1)
+    dimn <- list(NULL, c("RemoteType", "GithubUsername", "GithubRepo", "GithubSHA1"))
+    metamat <- matrix(meta, nrow = 1, dimnames = dimn)
+    dcf <- cbind(dcf, metamat)
+    write.dcf(dcf, file = desc)
+    return(TRUE)
+}
