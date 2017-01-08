b83e9967e75c6bfe547bc2c7a6d1ee0f598d54e6
Imanuel Costigan <i.costigan@me.com>
2017-01-08 14:08:20 +1100

    Documented

--- R/install-bitbucket.R
+++ R/install-bitbucket.R
@@ -1,3 +1,33 @@
+#' @title Install R package from Bitbucket
+#' @description \code{install_bitbucket} allows users to install R packages hosted on Bitbucket without needing to install or load the heavy dependencies required by devtools. ghit provides a drop-in replacement that provides (almost) identical functionality to \code{devtools::install_bitbucket()}.
+#' @param repo A character vector naming one or more GitHub repository containing an R package to install (e.g., \dQuote{imanuelcostigan/devtest}), or optionally a branch (\dQuote{imanuelcostigan/devtest[dev]}), a reference (\dQuote{imanuelcostigan/devtest@309fa0a}), tag (\dQuote{imanuelcostigan/devtest@v0.1}), or subdirectory (\dQuote{imanuelcostigan/samplepackage/R}). These arguments can be placed in any order and in any combination (e.g., \dQuote{imanuelcostigan/devtest[master]@abc123/R}).
+#' @param host A character string naming a host, to enable installation of enterprise-hosted Bitbucket Server packages.
+#' @param credentials}{An argument passed to the \code{credentials} argument to \code{\link[git2r]{clone}}. See \code{\link[git2r]{cred_user_pass}} or \code{\link[git2r]{cred_ssh_key}}.
+#' @param build_args A character string used to control the package build, passed to \code{R CMD build}.
+#' @param build_vignettes A logical specifying whether to build package vignettes, passed to \code{R CMD build}. Can be slow. Note: The default is \code{TRUE}, unlike in \code{devtools::install_github()}.
+#' @param uninstall A logical specifying whether to uninstall previous installations using \code{\link[utils]{remove.packages}} before attempting install. This is useful for installing an older version of a package than the one currently installed.
+#' @param verbose A logical specifying whether to print details of package building and installation.
+#' @param repos A character vector specifying one or more URLs for CRAN-like repositories from which package dependencies might be installed. By default, value is taken from \code{options("repos")} or set to the CRAN cloud repository.
+#' @param type A character vector passed to the \code{type} argument of \code{\link[utils]{install.packages}}.
+#' @param dependencies A character vector specifying which dependencies to install (of \dQuote{Depends}, \dQuote{Imports}, \dQuote{Suggests}, etc.).
+#' @param \dots Additional arguments to control installation of package, passed to \code{\link[utils]{install.packages}}.
+#' @return A named character vector of R package versions installed.
+#' @author Imanuel Costigan
+#' @examples
+#' \dontrun{
+#' tmp <- file.path(tempdir(), "tmplib")
+#' dir.create(tmp)
+#' # install a single package. Multiple package install is also supported.
+#' install_bitbucket("imanuelcostigan/devtest", lib = tmp)
+#'
+#' # cleanup
+#' unlink(tmp, recursive = TRUE)
+#' }
+#' @importFrom git2r init clone config commits remote_ls
+#' @importFrom utils install.packages installed.packages remove.packages packageVersion compareVersion capture.output
+#' @importFrom tools write_PACKAGES
+#' @export
+
 install_bitbucket <- function(repo, host = "bitbucket.org", credentials = NULL,
     build_args = NULL, build_vignettes = TRUE, uninstall = FALSE,
     verbose = FALSE,
